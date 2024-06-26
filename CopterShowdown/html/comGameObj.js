0a91ef805de7a298a3338a54cbd26fc74aeb5c40
truemaxdh <truemaxdh@gmail.com>
2024-06-26 17:21:42 +0900

    Create comGameObj.js

---
+++ CopterShowdown/html/comGameObj.js
@@ -0,0 +1,44 @@
+class gameobj {
+    constructor(cx, cy, r, dx, dy) {
+        this.center = new Vector2D(cx, cy);
+        this.r = r;
+        this.m = r * r;
+        this.speed = new Vector2D(dx, dy);
+        this.accel = new Vector2D(0, 0);
+        this.prev = null;
+        this.next = null;
+    }
+    
+    move() {
+        this.speed.add(this.accel);
+        this.hitTheWall();
+        this.center.add(this.speed);
+        if (this.next != null) {
+            this.next.move();
+        }
+    }
+
+    hitTheWall() {
+        let newCenter = this.center.clone();
+        newCenter.add(this.speed);
+        if ((newCenter.v1 - this.r) < 0 && this.speed.v1 < 0 || 
+            (newCenter.v1 + this.r) > gameCanvas.w && this.speed.v1 > 0) {
+                this.speed.v1 *= -1;
+            }
+            
+        if ((newCenter.v2 - this.r) < 0 && this.speed.v2 < 0 || 
+            (newCenter.v2 + this.r) > gameCanvas.h && this.speed.v2 > 0) {
+                this.speed.v2 *= -1;
+            }
+    }
+
+    render() {
+        if (this.next != null) {
+            this.next.render();
+        }
+    }
+
+    collide(other) {
+        return this.center.calcDist(other.center) < (this.r + other.r);
+    }
+}
